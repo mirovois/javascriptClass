33b5c868fd0cabb981ffd40fc16b68475d038bfe
Miro <mirovois@gmail.com>
2019-01-24 21:32:34 -0500

    Html events example

---
+++ w2/js/script.js
@@ -0,0 +1,48 @@
+'use strict'
+// console.log('foo bar');
+// EXERCICE_1//
+
+// let salary = 65000;
+//
+// function calculateTax() {
+//     if (salary<42000) {
+//       return 0;
+//     } else {
+//       return salary*0.2;
+//     }
+// }
+//
+// let tax = calculateTax(salary);
+// console.log(salary - tax);
+
+// EXERCICE_2- DAY SELECTION
+
+// let day = "Saturday";
+// function isWorkingDay(day) {
+//   if (day == "Monday" || day == "Tuesday" || day == "Wednesday" ||
+//       day == "Thursday" || day == "Friday"  ) {
+//         return true;
+//       }
+//   else if (day == "Saturday" || day == "Sunday") {
+//       return false;
+//   }
+// }
+
+
+// ---EXERCISE_3---
+
+// function average (n1, n2, n3) {
+//   return (n1+n2+n3)/3;
+// }
+//
+//  const avg = average(1,2,3);
+// console.log(avg);
+
+// ---EXERCISE_4---
+
+// let average = function (n1=0, n2=1, n3=0) {
+//   return (n1+n2+n3)/3;
+// }
+//
+//  const avg = average3(1,2,3);
+// console.log(avg);
